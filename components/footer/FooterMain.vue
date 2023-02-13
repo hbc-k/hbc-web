4e9d19a37a4a1aa9ac05af1a91ba77a9b5be8dc7
Per-Terra <53376589+Per-Terra@users.noreply.github.com>
2023-02-14 01:51:08 +0900

    write copyright in footer

--- components/footer/FooterMain.vue
+++ components/footer/FooterMain.vue
@@ -1,3 +1,9 @@
 <template>
-  <div>This is footer.</div>
+  <div class="bg-gray-700 text-white backdrop-blur">
+    <div class="mx-auto max-w-6xl items-center py-4 px-6">
+      <div class="text-xs">
+        <p class="text-right">&copy; 2023 Hiroshima Broadcasting Club</p>
+      </div>
+    </div>
+  </div>
 </template>
