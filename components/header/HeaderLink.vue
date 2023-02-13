c4cb10c47e3afad45fe6589c7f79e050af5146f7
Per-Terra <53376589+Per-Terra@users.noreply.github.com>
2023-02-14 01:45:39 +0900

    add HeaderLink component and header links

---
+++ components/header/HeaderLink.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface Props {
+  to: string;
+}
+const Props = defineProps<Props>();
+</script>
+
+<template>
+  <NuxtLink
+    :to="to"
+    class="relative inline-block before:absolute before:bottom-[-1px] before:left-[-1px] before:z-[100] before:h-[1px] before:w-[calc(100%_+_2px)] before:origin-right before:scale-x-0 before:bg-white before:transition before:duration-200 before:content-[''] before:hover:origin-left before:hover:scale-x-100"
+  >
+    <slot />
+  </NuxtLink>
+</template>
--- components/header/HeaderMain.vue
+++ components/header/HeaderMain.vue
@@ -1,3 +1,29 @@
+<script setup lang="ts">
+const links: { name: string; to: string }[] = [
+  { name: 'ホーム', to: '/' },
+  { name: '放送部について', to: '/about' },
+  { name: 'お問い合わせ', to: '/contact' },
+];
+</script>
+
 <template>
-  <div>This is header.</div>
+  <div class="bg-red-700 bg-opacity-95 text-white shadow-md backdrop-blur">
+    <div class="mx-auto flex max-w-6xl items-center py-4 px-6">
+      <NuxtLink to="/" class="flex items-center">
+        <TheLogo class="mr-2 h-8 w-auto shrink-0 fill-white" />
+        <div class="hidden text-xs sm:block">
+          <p>県広放送部</p>
+          <p>公式Webサイト「HBC Web」</p>
+        </div>
+      </NuxtLink>
+      <div class="ml-auto hidden sm:block">
+        <ul>
+          <li v-for="link in links" :key="link.name" class="mr-4 inline-block">
+            <HeaderLink to="/">{{ link.name }}</HeaderLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ml-auto block sm:hidden"><font-awesome-icon icon="fa-solid fa-bars" class="text-2xl" /></div>
+    </div>
+  </div>
 </template>
