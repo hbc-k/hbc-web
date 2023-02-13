460d338f6eb40a67bac30028cefc7af38b50fec6
Per-Terra <53376589+Per-Terra@users.noreply.github.com>
2023-02-14 01:54:01 +0900

    make header and footer sticky

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    <HeaderMain />
-    <slot />
-    <FooterMain />
+  <div class="flex min-h-screen flex-col">
+    <div class="sticky top-0 z-40">
+      <HeaderMain />
+    </div>
+    <div class="flex-grow">
+      <slot />
+    </div>
+    <div>
+      <FooterMain />
+    </div>
   </div>
 </template>
