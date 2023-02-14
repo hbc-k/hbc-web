9265f859711f9c2c6946a014e8b0315e052c5493
Per-Terra <53376589+Per-Terra@users.noreply.github.com>
2023-02-14 19:25:15 +0900

    create index page

---
+++ components/button/ButtonLinkRightArrow.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface Props {
+  to: string;
+}
+const Props = defineProps<Props>();
+</script>
+
+<template>
+  <NuxtLink
+    :to="to"
+    class="inline-block transform overflow-hidden rounded-md border border-red-700 px-4 py-2 text-sm font-bold text-red-700 transition duration-200 before:absolute before:top-0 before:left-0 before:h-full before:w-full before:origin-left before:scale-x-0 before:bg-red-700 before:transition before:duration-200 before:content-[''] after:relative after:bottom-[1px] after:z-10 after:ml-2 after:inline-block after:h-2 after:w-2 after:rotate-45 after:border-t after:border-r after:border-red-700 after:transition after:duration-200 after:content-[''] hover:text-white before:hover:scale-x-100 after:hover:border-white after:hover:text-white"
+  >
+    <span class="relative z-10"><slot /></span>
+  </NuxtLink>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,49 @@
 <template>
-  <div>This is index.</div>
+  <section class="pointer-events-none bg-[url('/assets/img/building.webp')] bg-cover shadow-md">
+    <div class="bg-black bg-opacity-40 backdrop-blur">
+      <div class="mx-auto max-w-6xl px-4 py-8 md:px-8">
+        <div class="text-center text-white md:flex">
+          <h1 class="text-5xl font-bold leading-snug md:text-left md:text-6xl md:leading-snug">Hiroshima<br />Broadcasting<br />Club</h1>
+          <p class="mt-auto ml-auto pt-4 md:text-right md:text-base">
+            広島県立広島中学校・広島高等学校 放送部<br />公式Webサイト「HBC Web」へようこそ！
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="mx-auto max-w-6xl px-4 py-10 md:px-8">
+      <div class="mb-6 text-center text-gray-800">
+        <h1><span class="text-2xl font-bold">Pickup!</span><span class="ml-4">放送部からのお知らせ</span></h1>
+      </div>
+    </div>
+  </section>
+  <section class="bg-gray-100">
+    <div class="mx-auto max-w-4xl px-4 py-10 md:px-8">
+      <div class="mb-10 text-center text-gray-800">
+        <h1 class="text-xl font-bold">放送部について</h1>
+      </div>
+      <div class="mb-10 text-center">
+        <h2 class="mb-10 text-3xl leading-relaxed text-red-700">
+          広島県立広島中学校・広島高等学校 放送部は、<br />県広の様々な校内行事を支えています。
+        </h2>
+        <p class="text-lg">
+          2004年（平成16年）の開校以来、校内の行事や式典における司会・撮影・音響、学校紹介ビデオをはじめとした各種ビデオの制作、お昼の休憩時間中の放送、各種大会への参加など、さまざまな活動を行っています。
+        </p>
+      </div>
+      <div class="text-center">
+        <ButtonLinkRightArrow to="/about">もっと詳しく</ButtonLinkRightArrow>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="mx-auto max-w-6xl px-4 py-10 md:px-8">
+      <div class="mb-6 text-center text-gray-800">
+        <h1><span class="text-2xl font-bold">Blog</span><span class="ml-4">部員が活動の様子をお届け！</span></h1>
+      </div>
+      <div class="text-center">
+        <ButtonLinkRightArrow to="/">もっと見る</ButtonLinkRightArrow>
+      </div>
+    </div>
+  </section>
 </template>
