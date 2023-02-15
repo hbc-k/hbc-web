fc681b8ddac585b29f9da461d1e1c2d8b38b8fe9
Per-Terra <53376589+Per-Terra@users.noreply.github.com>
2023-02-15 11:14:29 +0900

    update InfoCard and change index page design

--- components/InfoCard.vue
+++ components/InfoCard.vue
@@ -1,25 +1,28 @@
 <script setup lang="ts">
 interface Props {
-  title: string;
-  description: string;
-  url: string;
-  newTab?: boolean;
-  date: string;
+  info: {
+    title: string;
+    description: string;
+    url: string;
+    newTab?: boolean;
+    date: string;
+    pin?: boolean;
+  };
 }
 const Props = defineProps<Props>();
-const target = Props.newTab ? '_blank' : '_self';
-const dateString = new Date(Props.date).toLocaleDateString('sv-SE');
+const target = Props.info.newTab ? '_blank' : '_self';
+const dateString = new Date(Props.info.date).toLocaleDateString('sv-SE');
 </script>
 
 <template>
-  <NuxtLink :to="url" :target="target">
+  <NuxtLink :to="info.url" :target="target">
     <div class="group flex h-full flex-col rounded border px-4 py-5 shadow transition hover:border-red-200 hover:text-red-600">
       <div class="mb-3 px-1">
         <h2 class="mb-2 text-xl font-bold">
-          {{ title }}
+          {{ info.title }}
         </h2>
         <p>
-          {{ description }}
+          {{ info.description }}
         </p>
       </div>
       <div class="mt-auto border-t px-1 pt-3 text-sm text-gray-600 transition group-hover:border-red-200 group-hover:text-red-400">
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
 const { data } = await useAsyncData('info', () => queryContent('/data/info').findOne());
-const infos: { title: string; description: string; url: string; newTab?: boolean; date: string }[] = (() => {
+const infos: { title: string; description: string; url: string; newTab?: boolean; date: string; pin?: boolean }[] = (() => {
   if (data.value) {
     return data.value.body;
   } else {
     return [];
   }
 })();
+const pinnedInfos = infos.filter((info) => info.pin);
 </script>
 
 <template>
   <section class="pointer-events-none bg-[url('/assets/img/building.webp')] bg-cover shadow-md">
     <div class="bg-black bg-opacity-40 backdrop-blur">
-      <div class="mx-auto max-w-6xl px-4 py-8 md:px-8">
+      <div class="mx-auto max-w-6xl px-4 py-8 sm:px-8">
         <div class="text-center text-white md:flex">
           <h1 class="text-5xl font-bold leading-snug md:text-left md:text-6xl md:leading-snug">Hiroshima<br />Broadcasting<br />Club</h1>
           <p class="mt-auto ml-auto pt-4 md:text-right md:text-base">
@@ -23,31 +24,26 @@ const infos: { title: string; description: string; url: string; newTab?: boolean
     </div>
   </section>
   <section>
-    <div class="mx-auto max-w-6xl px-4 py-10 md:px-8">
+    <div class="mx-auto max-w-6xl px-4 py-10 sm:px-8">
       <div class="mb-6 text-center text-gray-800">
         <h1><span class="text-2xl font-bold">Info</span><span class="ml-4">放送部からのお知らせ</span></h1>
       </div>
       <div class="grid grid-cols-1 gap-4 lg:grid-cols-2">
-        <InfoCard
-          v-for="info in infos"
-          :key="info.title"
-          :title="info.title"
-          :description="info.description"
-          :url="info.url"
-          :new-tab="info.newTab"
-          :date="info.date"
-        />
+        <InfoCard v-for="info in pinnedInfos" :key="info.title" :info="info" />
       </div>
     </div>
   </section>
   <section class="bg-gray-100">
-    <div class="mx-auto max-w-4xl px-4 py-10 md:px-8">
+    <div class="mx-auto max-w-4xl px-4 py-10 sm:px-8">
       <div class="mb-10 text-center text-gray-800">
         <h1 class="text-xl font-bold">放送部について</h1>
       </div>
       <div class="mb-10 text-center">
         <h2 class="mb-10 text-3xl leading-relaxed text-red-700">
-          広島県立広島中学校・広島高等学校 放送部は、<br />県広の様々な校内行事を支えています。
+          <span class="inline-block">広島県立広島中学校・</span><span class="inline-block">広島高等学校 放送部は、</span><br /><span
+            class="inline-block"
+            >県広の様々な校内行事を</span
+          ><span class="inline-block">支えています。</span>
         </h2>
         <p class="text-lg">
           2004年（平成16年）の開校以来、校内の行事や式典における司会・撮影・音響、学校紹介ビデオをはじめとした各種ビデオの制作、お昼の休憩時間中の放送、各種大会への参加など、さまざまな活動を行っています。
