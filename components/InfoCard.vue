1a6f19ed8dbbe6f26e28197babbed278add15cb6
Per-Terra <53376589+Per-Terra@users.noreply.github.com>
2023-02-16 01:34:29 +0900

    update InfoCard design

--- components/InfoCard.vue
+++ components/InfoCard.vue
@@ -1,32 +1,35 @@
 <script setup lang="ts">
 interface Props {
-  info: {
-    title: string;
-    description: string;
-    url: string;
-    newTab?: boolean;
-    date: string;
-    pin?: boolean;
-  };
+  title: string;
+  description: string;
+  url: string;
+  newTab?: boolean;
+  date: string;
+  pin?: boolean;
 }
 const Props = defineProps<Props>();
-const target = Props.info.newTab ? '_blank' : '_self';
-const dateString = new Date(Props.info.date).toLocaleDateString('sv-SE');
+const target = Props.newTab ? '_blank' : '_self';
+const dateString = new Date(Props.date).toLocaleDateString('sv-SE');
 </script>
 
 <template>
-  <NuxtLink :to="info.url" :target="target">
-    <div class="group flex h-full flex-col rounded border px-4 py-5 shadow transition hover:border-red-200 hover:text-red-600">
+  <NuxtLink :to="url" :target="target">
+    <div
+      class="group relative flex h-full flex-col rounded-lg border px-4 py-5 shadow-md transition duration-300 hover:-translate-y-1 hover:border-red-200 hover:text-red-600"
+    >
       <div class="mb-3 px-1">
         <h2 class="mb-2 text-xl font-bold">
-          {{ info.title }}
+          {{ title }}
         </h2>
         <p>
-          {{ info.description }}
+          {{ description }}
+          <span v-if="newTab" class="ml-1">
+            <font-awesome-icon icon="fa-solid fa-arrow-up-right-from-square" />
+          </span>
         </p>
       </div>
       <div class="mt-auto border-t px-1 pt-3 text-sm text-gray-600 transition group-hover:border-red-200 group-hover:text-red-400">
-        <font-awesome-icon icon="fa-regular fa-calendar" />
+        <font-awesome-icon icon="fa-regular fa-clock" />
         <span class="ml-1">
           {{ dateString }}
         </span>
