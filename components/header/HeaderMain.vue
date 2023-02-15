c774650123ceca7b5704fc35462925619e6b3b5b
Per-Terra <53376589+Per-Terra@users.noreply.github.com>
2023-02-15 11:12:36 +0900

    change header design

--- components/header/HeaderMain.vue
+++ components/header/HeaderMain.vue
@@ -8,17 +8,17 @@ const links: { name: string; to: string }[] = [
 
 <template>
   <div class="bg-red-700 bg-opacity-95 text-white shadow-md backdrop-blur">
-    <div class="mx-auto flex max-w-6xl items-center py-4 px-6">
+    <div class="mx-auto flex max-w-6xl items-center px-4 py-4 sm:px-6">
       <NuxtLink to="/" class="flex items-center">
-        <TheLogo class="mr-2 h-8 w-auto shrink-0 fill-white" />
-        <div class="hidden text-xs sm:block">
+        <TheLogo class="mr-2 h-7 w-auto shrink-0 fill-white" />
+        <div class="hidden text-[0.7rem] leading-tight sm:block">
           <p>県広放送部</p>
           <p>公式Webサイト「HBC Web」</p>
         </div>
       </NuxtLink>
       <div class="ml-auto hidden sm:block">
         <ul>
-          <li v-for="link in links" :key="link.name" class="mr-4 inline-block">
+          <li v-for="link in links" :key="link.name" class="ml-4 inline-block">
             <HeaderLink to="/">{{ link.name }}</HeaderLink>
           </li>
         </ul>
