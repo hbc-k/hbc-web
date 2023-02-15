0f42036f5a0bc83b84a65588c4f334609d3f8015
Per-Terra <53376589+Per-Terra@users.noreply.github.com>
2023-02-16 01:36:04 +0900

    update design and add BlogCard

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,29 @@
 <script setup lang="ts">
-const { data } = await useAsyncData('info', () => queryContent('/data/info').findOne());
-const infos: { title: string; description: string; url: string; newTab?: boolean; date: string; pin?: boolean }[] = (() => {
-  if (data.value) {
-    return data.value.body;
-  } else {
-    return [];
-  }
-})();
-const pinnedInfos = infos.filter((info) => info.pin);
+import type { ParsedContent } from '@nuxt/content/dist/runtime/types';
+
+interface Info extends ParsedContent {
+  url: string;
+  newTab?: boolean;
+  date: string;
+  pin?: boolean;
+}
+const { data: pinnedInfos } = await useAsyncData('info', () => queryContent<Info>('info').where({ pin: true }).sort({ date: -1 }).find());
+
+interface BlogPost extends ParsedContent {
+  author: string;
+  category: string;
+  tags: string[];
+  coverImage: string;
+  createDate: string;
+  updateDate: string;
+}
+const { data: blogPosts } = await useAsyncData('articles', () => queryContent<BlogPost>('blog', 'posts').sort({ createDate: -1 }).limit(3).find());
 </script>
 
 <template>
   <section class="pointer-events-none bg-[url('/assets/img/building.webp')] bg-cover shadow-md">
     <div class="bg-black bg-opacity-40 backdrop-blur">
-      <div class="mx-auto max-w-6xl px-4 py-8 sm:px-8">
+      <div class="mx-auto max-w-7xl px-4 py-8 sm:px-8">
         <div class="text-center text-white md:flex">
           <h1 class="text-5xl font-bold leading-snug md:text-left md:text-6xl md:leading-snug">Hiroshima<br />Broadcasting<br />Club</h1>
           <p class="mt-auto ml-auto pt-4 md:text-right md:text-base">
@@ -24,28 +34,43 @@ const pinnedInfos = infos.filter((info) => info.pin);
     </div>
   </section>
   <section>
-    <div class="mx-auto max-w-6xl px-4 py-10 sm:px-8">
-      <div class="mb-6 text-center text-gray-800">
-        <h1><span class="text-2xl font-bold">Info</span><span class="ml-4">放送部からのお知らせ</span></h1>
+    <div class="mx-auto max-w-7xl px-4 pt-10 pb-14 sm:px-8">
+      <div class="mb-8 text-gray-800">
+        <div class="items-center text-center">
+          <span class="relative bottom-0.5 mr-4 text-2xl"><font-awesome-icon icon="fa-solid fa-bullhorn" /></span>
+          <h1 class="inline text-3xl uppercase">Information</h1>
+          <p class="mt-2">放送部からのお知らせ</p>
+        </div>
       </div>
       <div class="grid grid-cols-1 gap-4 lg:grid-cols-2">
-        <InfoCard v-for="info in pinnedInfos" :key="info.title" :info="info" />
+        <InfoCard
+          v-for="info in pinnedInfos"
+          :title="info.title ? info.title : 'undefinded'"
+          :description="info.description"
+          :url="info.url"
+          :newTab="info.newTab"
+          :date="info.date"
+          :pin="info.pin"
+        />
       </div>
     </div>
   </section>
-  <section class="bg-gray-100">
+  <section class="bg-gray-200">
     <div class="mx-auto max-w-4xl px-4 py-10 sm:px-8">
-      <div class="mb-10 text-center text-gray-800">
-        <h1 class="text-xl font-bold">放送部について</h1>
+      <div class="mb-10 text-gray-800">
+        <div class="items-center text-center">
+          <h1 class="inline text-3xl uppercase">About HBC</h1>
+          <p class="mt-2">放送部について</p>
+        </div>
       </div>
       <div class="mb-10 text-center">
-        <h2 class="mb-10 text-3xl leading-relaxed text-red-700">
-          <span class="inline-block">広島県立広島中学校・</span><span class="inline-block">広島高等学校 放送部は、</span><br /><span
+        <h2 class="text-3xl leading-relaxed text-red-700">
+          <span class="inline-block">広島県立広島中学校・</span><span class="inline-block">広島高等学校 放送部は</span><br /><span
             class="inline-block"
             >県広の様々な校内行事を</span
-          ><span class="inline-block">支えています。</span>
+          ><span class="inline-block">支えています</span>
         </h2>
-        <p class="text-lg">
+        <p class="mt-10 text-lg">
           2004年（平成16年）の開校以来、校内の行事や式典における司会・撮影・音響、学校紹介ビデオをはじめとした各種ビデオの制作、お昼の休憩時間中の放送、各種大会への参加など、さまざまな活動を行っています。
         </p>
       </div>
@@ -55,12 +80,31 @@ const pinnedInfos = infos.filter((info) => info.pin);
     </div>
   </section>
   <section>
-    <div class="mx-auto max-w-6xl px-4 py-10 md:px-8">
-      <div class="mb-6 text-center text-gray-800">
-        <h1><span class="text-2xl font-bold">Blog</span><span class="ml-4">部員が活動の様子をお届け！</span></h1>
+    <div class="mx-auto max-w-7xl px-4 py-10 md:px-8">
+      <div class="mb-8 text-gray-800">
+        <div class="items-center text-center">
+          <span class="relative bottom-0.5 mr-4 text-2xl"><font-awesome-icon icon="fa-solid fa-pen-clip" /></span>
+          <h1 class="inline text-3xl uppercase">Blog</h1>
+          <p class="mt-2">不定期更新</p>
+        </div>
+      </div>
+      <div class="mb-10 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <BlogCard
+          v-for="article in blogPosts"
+          :key="article.title"
+          :to="article._path!"
+          :title="article.title ? article.title : 'undefinded'"
+          :description="article.description"
+          :author="article.author"
+          :category="article.category"
+          :tags="article.tags"
+          :coverImage="article.coverImage"
+          :createDate="article.createDate"
+          :updateDate="article.updateDate"
+        />
       </div>
       <div class="text-center">
-        <ButtonLinkArrowRight to="/">もっと見る</ButtonLinkArrowRight>
+        <ButtonLinkArrowRight to="/blog">もっと見る</ButtonLinkArrowRight>
       </div>
     </div>
   </section>
