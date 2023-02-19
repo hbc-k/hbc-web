d3457f04d56fbd9871b1aa784d2b2eb6d3d9ec03
Per-Terra <53376589+Per-Terra@users.noreply.github.com>
2023-02-20 00:52:44 +0900

    improve header links

--- components/header/HeaderLink.vue
+++ components/header/HeaderLink.vue
@@ -7,30 +7,15 @@ const Props = defineProps<Props>();
 
 <template>
   <NuxtLink
-    v-if="to == '/' && $route.path != '/'"
     :to="to"
-    class="relative inline-block uppercase before:absolute before:bottom-[-1px] before:left-[-1px] before:z-[100] before:h-[1px] before:w-[calc(100%_+_2px)] before:origin-right before:scale-x-0 before:bg-white before:transition before:duration-200 before:content-[''] before:hover:origin-left before:hover:scale-x-100"
+    v-bind:class="{
+      'before:origin-left before:scale-x-100': !(($route.path !== '/' && to === '/') || !$route.path.startsWith(to)),
+      'before:origin-right before:scale-x-0 before:hover:origin-left before:hover:scale-x-100':
+        ($route.path !== '/' && to === '/') || !$route.path.startsWith(to),
+      'pointer-events-none': $route.path === to,
+    }"
+    class="relative inline-block uppercase before:absolute before:bottom-[-1px] before:left-[-1px] before:z-[100] before:h-[1px] before:w-[calc(100%_+_2px)] before:bg-white before:transition before:duration-200 before:content-['']"
   >
     <slot />
   </NuxtLink>
-  <NuxtLink
-    v-else-if="!$route.path.startsWith(to)"
-    :to="to"
-    class="relative inline-block uppercase before:absolute before:bottom-[-1px] before:left-[-1px] before:z-[100] before:h-[1px] before:w-[calc(100%_+_2px)] before:origin-right before:scale-x-0 before:bg-white before:transition before:duration-200 before:content-[''] before:hover:origin-left before:hover:scale-x-100"
-  >
-    <slot />
-  </NuxtLink>
-  <NuxtLink
-    v-else-if="to != $route.path"
-    :to="to"
-    class="relative inline-block uppercase before:absolute before:bottom-[-1px] before:left-[-1px] before:z-[100] before:h-[1px] before:w-[calc(100%_+_2px)] before:bg-white before:content-['']"
-  >
-    <slot />
-  </NuxtLink>
-  <span
-    v-else
-    class="pointer-events-none relative inline-block uppercase before:absolute before:bottom-[-1px] before:left-[-1px] before:z-[100] before:h-[1px] before:w-[calc(100%_+_2px)] before:bg-white before:content-['']"
-  >
-    <slot />
-  </span>
 </template>
