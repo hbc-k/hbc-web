2f42f149595cf1b1b5eb1ee2245862a10b035148
Per-Terra <53376589+Per-Terra@users.noreply.github.com>
2023-02-19 14:01:43 +0900

    update HeaderLink

--- components/header/HeaderLink.vue
+++ components/header/HeaderLink.vue
@@ -7,9 +7,16 @@ const Props = defineProps<Props>();
 
 <template>
   <NuxtLink
+    v-if="to != $route.path"
     :to="to"
     class="relative inline-block uppercase before:absolute before:bottom-[-1px] before:left-[-1px] before:z-[100] before:h-[1px] before:w-[calc(100%_+_2px)] before:origin-right before:scale-x-0 before:bg-white before:transition before:duration-200 before:content-[''] before:hover:origin-left before:hover:scale-x-100"
   >
     <slot />
   </NuxtLink>
+  <span
+    v-else
+    class="pointer-events-none relative inline-block uppercase before:absolute before:bottom-[-1px] before:left-[-1px] before:z-[100] before:h-[1px] before:w-[calc(100%_+_2px)] before:bg-white before:content-['']"
+  >
+    <slot />
+  </span>
 </template>
