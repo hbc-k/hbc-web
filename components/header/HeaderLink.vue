71cc3e32400bdbd585d03deca1d7deebfc57c145
Per-Terra <53376589+Per-Terra@users.noreply.github.com>
2023-02-19 15:39:20 +0900

    improve header links

--- components/header/HeaderLink.vue
+++ components/header/HeaderLink.vue
@@ -7,9 +7,23 @@ const Props = defineProps<Props>();
 
 <template>
   <NuxtLink
-    v-if="to != $route.path"
+    v-if="to == '/' && $route.path != '/'"
     :to="to"
     class="relative inline-block uppercase before:absolute before:bottom-[-1px] before:left-[-1px] before:z-[100] before:h-[1px] before:w-[calc(100%_+_2px)] before:origin-right before:scale-x-0 before:bg-white before:transition before:duration-200 before:content-[''] before:hover:origin-left before:hover:scale-x-100"
+  >
+    <slot />
+  </NuxtLink>
+  <NuxtLink
+    v-else-if="!$route.path.startsWith(to)"
+    :to="to"
+    class="relative inline-block uppercase before:absolute before:bottom-[-1px] before:left-[-1px] before:z-[100] before:h-[1px] before:w-[calc(100%_+_2px)] before:origin-right before:scale-x-0 before:bg-white before:transition before:duration-200 before:content-[''] before:hover:origin-left before:hover:scale-x-100"
+  >
+    <slot />
+  </NuxtLink>
+  <NuxtLink
+    v-else-if="to != $route.path"
+    :to="to"
+    class="relative inline-block uppercase before:absolute before:bottom-[-1px] before:left-[-1px] before:z-[100] before:h-[1px] before:w-[calc(100%_+_2px)] before:bg-white before:content-['']"
   >
     <slot />
   </NuxtLink>
