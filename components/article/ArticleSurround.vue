d6799319f6dfb40930054f77adee1325e5df2b38
Per-Terra <53376589+Per-Terra@users.noreply.github.com>
2023-02-20 03:41:23 +0900

    fix next and prev buttons text are not correct

--- components/article/ArticleSurround.vue
+++ components/article/ArticleSurround.vue
@@ -20,7 +20,7 @@ const [prev, next] = await queryContent('blog', 'posts').only(['_path', 'title']
     </NuxtLink>
     <div v-else class="flex h-16 items-center rounded-md border bg-white px-4 py-2 text-sm font-bold text-gray-500 transition duration-300">
       <font-awesome-icon icon="fa-solid fa-chevron-left" class="mr-2" />
-      <span>これより新しい記事はありません</span>
+      <span>これより古い記事はありません</span>
     </div>
     <NuxtLink
       v-if="next"
@@ -34,7 +34,7 @@ const [prev, next] = await queryContent('blog', 'posts').only(['_path', 'title']
       v-else
       class="flex h-16 items-center justify-end rounded-md border bg-white px-4 py-2 text-sm font-bold text-gray-500 transition duration-300"
     >
-      <span>これより古い記事はありません</span>
+      <span>これより新しい記事はありません</span>
       <font-awesome-icon icon="fa-solid fa-chevron-right" class="ml-2" />
     </div>
   </div>
