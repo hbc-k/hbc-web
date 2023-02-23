019420963ea0674658238816f90a5d1cc4e5a0f7
Per-Terra <53376589+Per-Terra@users.noreply.github.com>
2023-02-24 00:27:09 +0900

    change ogp default image format from webp to png

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@ useServerSeoMeta({
 });
 useSeoMeta({
   titleTemplate: 'HBC Web - %s',
-  ogImage: 'https://hbc-web.vercel.app/img/ogp_default.webp',
+  ogImage: 'https://hbc-web.vercel.app/img/ogp_default.png',
   ogType: 'website',
 });
 </script>
