663ad061d3d612d52fe846330f2aa418d76536fc
Per-Terra <53376589+Per-Terra@users.noreply.github.com>
2023-02-27 15:14:35 +0900

    change text size

--- pages/info/index.vue
+++ pages/info/index.vue
@@ -36,8 +36,8 @@ const { data: archivedInfos } = await useAsyncData('archivedInfos', () =>
         <div class="px-4 py-10 sm:px-6 lg:px-8">
           <div class="mb-8 text-gray-800">
             <div class="items-center text-center">
-              <span class="relative bottom-0.5 mr-4 text-2xl"><font-awesome-icon icon="fa-solid fa-thumbtack" /></span>
-              <h1 class="inline text-3xl uppercase">掲載中のお知らせ</h1>
+              <span class="relative bottom-0.5 mr-3 text-xl"><font-awesome-icon icon="fa-solid fa-thumbtack" /></span>
+              <h1 class="inline text-2xl uppercase">掲載中のお知らせ</h1>
             </div>
           </div>
           <div class="grid grid-cols-1 gap-4 lg:grid-cols-2">
@@ -51,9 +51,9 @@ const { data: archivedInfos } = await useAsyncData('archivedInfos', () =>
         <div class="px-4 py-10 sm:px-6 lg:px-8">
           <div class="mb-8 text-gray-800">
             <div class="items-center text-center">
-              <span class="relative bottom-0.5 mr-4 text-2xl"><font-awesome-icon icon="fa-solid fa-box-archive" /></span>
-              <h1 class="inline text-3xl uppercase">過去のお知らせ</h1>
-              <p class="mt-2">※掲載当時の情報です。現在の情報と異なる場合があります。</p>
+              <span class="relative bottom-0.5 mr-3 text-xl"><font-awesome-icon icon="fa-solid fa-box-archive" /></span>
+              <h1 class="inline text-2xl uppercase">過去のお知らせ</h1>
+              <p class="mt-2 text-sm">※掲載当時の情報です。現在の情報と異なる場合があります。</p>
             </div>
           </div>
           <div class="grid grid-cols-1 gap-4 lg:grid-cols-2">
