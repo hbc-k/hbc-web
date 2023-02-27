ed2880c5598d78fae79fd3df2b7aa51ba9927023
Per-Terra <53376589+Per-Terra@users.noreply.github.com>
2023-02-27 19:12:07 +0900

    customize nuxt content behavior 見出しタグの中身がanchorになっているとanchor用のCSSが適用されるため、anchorを除去

---
+++ components/content/ProseH2.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2 :id="id">
+    <slot />
+  </h2>
+</template>
+
+<script setup lang="ts">
+defineProps<{ id?: string }>();
+</script>
---
+++ components/content/ProseH6.vue
@@ -0,0 +1,9 @@
+<template>
+  <h6 :id="id">
+    <slot />
+  </h6>
+</template>
+
+<script setup lang="ts">
+defineProps<{ id?: string }>();
+</script>
