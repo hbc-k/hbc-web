99c307b727174d1f3da583d7ad47de2235c150c7
Per-Terra <53376589+Per-Terra@users.noreply.github.com>
2023-03-01 22:09:25 +0900

    optimize page header link behavior

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -25,8 +25,16 @@ onMounted(() => {
     <div class="pointer-events-auto bg-black bg-opacity-40 backdrop-blur">
       <div class="mx-auto max-w-7xl pt-[60px]">
         <div class="items-end justify-between py-6 text-center text-white sm:flex sm:px-6 lg:px-8">
-          <NuxtLink v-if="to != $route.path" :to="to" class="text-4xl uppercase hover:underline sm:text-left sm:text-5xl"><slot /></NuxtLink>
-          <div v-else class="text-4xl uppercase sm:text-left sm:text-5xl"><slot /></div>
+          <NuxtLink
+            v-bind:class="{
+              'hover:underline': to != $route.path,
+              'pointer-events-none': to == $route.path,
+            }"
+            :to="to"
+            class="text-4xl uppercase sm:text-left sm:text-5xl"
+          >
+            <slot />
+          </NuxtLink>
           <p class="mt-3 sm:mt-0 sm:text-right"><slot name="detail" /></p>
         </div>
       </div>
